dadcce809078948b039a2c5ab1215b5bfe0a8bf8
Daniel Viotor <danielvafranca@gmail.com>
2025-01-11 12:54:56 +0000

    Adicionado arquivo do git ignore

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fe5dd64db263328b142f026c29de3718127194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926f22c8c48978d7d3bdecbcb53ccb4094cd60dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
